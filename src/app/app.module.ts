fd468da0af851b0e4400a5e8a970b1bb39e6aa79
donkeykong91 <darylnator@gmail.com>
2020-01-03 10:51:37 -0800

    added dropdown component in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DropdownComponent } from './dropdown/dropdown.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
